834a7098db038cf3ed45e7145c18e673e29f873d
Kris Long <corpkris@gmail.com>
2021-06-02 18:24:23 +0200

    created test route

---
+++ src/routes/auth.ts
@@ -0,0 +1,30 @@
+import { S_IFMT } from 'constants';
+import { Request, Response, Router } from 'express';
+import { validate } from 'class-validator';
+
+import { User } from '../entities/User';
+
+const register = async (req: Request, res: Response) => {
+  const { email, username, password } = req.body;
+
+  try {
+    //TODO Validate Data
+    //TODO Create User
+    const user = new User({ email, username, password });
+    const errors = await validate(user);
+    if (errors.length > 0) return res.status(400).json({ errors });
+
+    await user.save;
+
+    //TODO Return User
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
+
+const router = Router();
+router.post('/register', register);
+
+export default router;
