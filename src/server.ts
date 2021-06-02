c5e28864d2db537d0d4c6a79dbd869f9058c4c53
Kris Long <corpkris@gmail.com>
2021-06-02 19:01:26 +0200

    added trim middleware to remove blank spaces

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import express from 'express';
 import morgan from 'morgan'; //http request logger for node
 
 import authRoutes from './routes/auth';
+import trim from './middleware/trim';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(trim);
 
 app.get('/', (_, res) => res.send('Hello World'));
 app.use('/api/auth', authRoutes);
