bfe3a4869e3e1fe29a5cdb89c657addfd6b7b86e
Kris Long <corpkris@gmail.com>
2021-06-02 19:36:59 +0200

    added password hashing bcrypt

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,4 @@
-import { IsEmail, MinLength } from 'class-validator';
+import { IsEmail, Length, MinLength } from 'class-validator';
 
 import {
   Entity,
@@ -8,7 +8,11 @@ import {
   Index,
   CreateDateColumn,
   UpdateDateColumn,
+  BeforeInsert,
 } from 'typeorm';
+
+import bcrypt from 'bcrypt';
+import { classToPlain, Exclude } from 'class-transformer';
 
 @Entity('users')
 export class User extends BaseEntity {
@@ -17,19 +21,24 @@ export class User extends BaseEntity {
     Object.assign(this, user);
   }
 
+  @Exclude() //id number is hidden from res
   @PrimaryGeneratedColumn()
   id: number;
 
   @Index()
   @IsEmail()
+  @Length(3, 254, { message: 'Please provide a valid email address.' })
   @Column({ unique: true })
   email: string;
 
   @Index()
-  @MinLength(3, { message: 'Username must be more than 3 characters.' })
+  @Length(3, 33, {
+    message: 'Username must be between 3 and 33 characters long.',
+  })
   @Column({ unique: true })
   username: string;
 
+  @Exclude() // hashed pw is hidden from res
   @Column()
   @MinLength(6, { message: 'Password must be more 6 characters' })
   password: string;
@@ -39,4 +48,13 @@ export class User extends BaseEntity {
 
   @UpdateDateColumn()
   updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  toJSON() {
+    return classToPlain(this);
+    //
+  }
 }
