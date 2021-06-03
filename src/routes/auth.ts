c1ce2bc57ef03a64cbbd0ceff956948c67d0ceca
Kris Long <corpkris@gmail.com>
2021-06-03 12:52:02 +0200

    added JWT and ENV support, cooky policy

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,8 @@
 import { Request, Response, Router } from 'express';
-import { validate } from 'class-validator';
+import { isEmpty, validate } from 'class-validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 import { User } from '../entities/User';
 
@@ -7,7 +10,7 @@ const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
   try {
-    //TODO Validate Data
+    //Validate Data
     let errors: any = {};
     const emailUser = await User.findOne({ email });
     const usernameUser = await User.findOne({ username });
@@ -19,14 +22,14 @@ const register = async (req: Request, res: Response) => {
       return res.status(400).json(errors);
     }
 
-    //TODO Create User
+    //Create User
     const user = new User({ email, username, password });
     errors = await validate(user);
     if (errors.length > 0) return res.status(400).json({ errors });
 
     await user.save();
 
-    //TODO Return User
+    //Return User
     return res.json(user);
   } catch (err) {
     console.log(err);
@@ -34,7 +37,43 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = 'Please enter a username';
+    if (isEmpty(password)) errors.password = 'Please enter a password';
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (!passwordMatches) {
+      return res.status(401).json({ password: 'Password is incorrect' });
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET); //key is in src/.env
+    res.set(
+      'set-cookie',
+      cookie.serialize('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // enable https if node_env is set to production
+        sameSite: 'strict',
+        maxAge: 28800, //cookie expires in 8 hours, change in production
+        path: '/',
+      })
+    );
+    return res.json({ user });
+  } catch (err) {}
+};
+
 const router = Router();
+router.post('/login', login);
 router.post('/register', register);
 
 export default router;
