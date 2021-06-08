aafc16e4b1e7ee7f46ded2ea62ec2aeb3d077006
Kris Long <corpkris@gmail.com>
2021-06-08 18:21:37 +0200

    added cors support to backend server

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import morgan from 'morgan'; //http request logger for node
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(trim);
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (_, res) => res.send('Hello World'));
 app.use('/api/auth', authRoutes);
