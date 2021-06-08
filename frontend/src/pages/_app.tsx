6ae610725b8a6dd036ac243a76ffe24197727c4e
Kris Long <corpkris@gmail.com>
2021-06-08 18:20:27 +0200

    added axios to global app + baseURL

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import Axios from 'axios';
+
+Axios.defaults.baseURL = 'http://localhost:5000/api';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
