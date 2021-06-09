95a2a5030a5ed73c64631f85f379ccf3ed0714c1
Kris Long <corpkris@gmail.com>
2021-06-09 15:01:43 +0200

    added cors config, linked to .env file

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,13 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(trim);
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.get('/', (_, res) => res.send('Hello World'));
 app.use('/api/auth', authRoutes);
