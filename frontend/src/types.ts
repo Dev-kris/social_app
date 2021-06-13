0854c0bf583db055d91bc8787880afbf14d4e021
Kris Long <corpkris@gmail.com>
2021-06-13 16:01:44 +0200

    created Sub interface

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -20,3 +20,19 @@ export interface User {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface Sub {
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  title: string;
+  description: string;
+  imageUrn: string;
+  bannerUrn: string;
+  username: string;
+  posts: Post[];
+
+  // virtual fields
+  imageUrl: string;
+  bannerUrl: string;
+}
