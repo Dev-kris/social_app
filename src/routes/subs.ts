88961faac74806aced5bec3bb91cee1df898e833
Kris Long <corpkris@gmail.com>
2021-06-13 12:41:29 +0200

    added subs route/logic, ordering

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -5,6 +5,7 @@ import { getRepository } from 'typeorm';
 import Sub from '../entities/Sub';
 import auth from '../middleware/auth';
 import user from '../middleware/user';
+import Post from '../entities/Posts';
 
 const createSub = async (req: Request, res: Response) => {
   const { name, title, description } = req.body;
@@ -44,8 +45,31 @@ const createSub = async (req: Request, res: Response) => {
   }
 };
 
+const getSub = async (req: Request, res: Response) => {
+  const name = req.params.name;
+
+  try {
+    const sub = await Sub.findOneOrFail({ name });
+    const posts = await Post.find({
+      where: { sub },
+      order: { createdAt: 'DESC' },
+      relations: ['comments', 'votes'],
+    });
+
+    sub.posts = posts;
+
+    if (res.locals.user) {
+      sub.posts.forEach((p) => p.setUserVote(res.locals.user));
+    }
+
+    return res.json(sub);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ sub: 'Sub not found. ' });
+  }
+};
 const router = Router();
 
 router.post('/', user, auth, createSub);
-
+router.get('/:name', user, getSub);
 export default router;
