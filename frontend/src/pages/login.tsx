5f2da04356e97c338708600f6eecac557b6582c5
Kris Long <corpkris@gmail.com>
2021-06-14 10:46:25 +0200

    changed login redirect to previous page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -29,7 +29,7 @@ export default function Register() {
 
       dispatch('LOGIN', res.data);
 
-      router.push('/');
+      router.back();
     } catch (err) {
       setErrors(err.response.data);
     }
