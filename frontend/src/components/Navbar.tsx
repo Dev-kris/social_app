f703fb5cccded5c6e067601347deddf0773ce9fc
Kris Long <corpkris@gmail.com>
2021-06-15 11:41:20 +0200

    added search functionality to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link';
 import Axios from 'axios';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { useAuthState, useAuthDispatch } from '../context/auth';
 import Logo from '../images/icon.svg';
+import { Sub } from '../types';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Navbar: React.FC = () => {
+  const [name, setName] = useState('');
   const { authenticated, loading } = useAuthState();
   const dispatch = useAuthDispatch();
+  const [subs, setSubs] = useState<Sub[]>([]);
+  const [timer, setTimer] = useState(null);
+
+  const router = useRouter();
 
   const logout = () => {
     Axios.get('/auth/logout')
@@ -15,6 +23,33 @@ const Navbar: React.FC = () => {
         window.location.reload();
       })
       .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    if (name.trim() === '') {
+      setSubs([]);
+      return;
+    }
+    searchSubs();
+  }, [name]);
+
+  const searchSubs = async () => {
+    clearTimeout(timer);
+    setTimer(
+      setTimeout(async () => {
+        try {
+          const { data } = await Axios.get(`/subs/search/${name}`);
+          setSubs(data);
+          console.log(data);
+        } catch (err) {
+          console.log(err);
+        }
+      }, 250)
+    );
+  };
+  const goToSub = (subName: string) => {
+    router.push(`/r/${subName}`);
+    setName(' ');
   };
   return (
     <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
@@ -28,14 +63,41 @@ const Navbar: React.FC = () => {
           <Link href="/">Groupomania</Link>
         </span>
       </div>
-      <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+      {/* Search Field */}
+      <div className="relative flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
         <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
         <input
           type="text"
           className="py-1 pr-3 bg-transparent rounded outline-none w-160 focus:"
           placeholder="Search"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
+        <div
+          className="absolute left-0 right-0 bg-white"
+          style={{ top: '100%' }}
+        >
+          {subs?.map((sub) => (
+            <div
+              className="flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200"
+              onClick={() => goToSub(sub.name)}
+            >
+              <Image
+                src={sub.imageUrl}
+                alt="sub"
+                className="rounded-full"
+                height={(8 * 16) / 4}
+                width={(8 * 16) / 4}
+              ></Image>
+              <div className="ml-4 text-sm">
+                <p className="font-medium">{sub.name}</p>
+                <p className="text-gray-600">{sub.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      {/* Authentication Buttons */}
       <div className="flex">
         {!loading &&
           (authenticated ? (
--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -138,10 +138,32 @@ const uploadSubImage = async (req: Request, res: Response) => {
   return res.json({ success: true });
 };
 
+const searchSubs = async (req: Request, res: Response) => {
+  try {
+    const name = req.params.name;
+    if (isEmpty(name)) {
+      return res.status(400).json({ error: 'Name must not be empty' });
+    }
+
+    const subs = await getRepository(Sub)
+      .createQueryBuilder()
+      .where('LOWER(name) LIKE :name', {
+        name: `${name.toLocaleLowerCase().trim()}%`,
+      })
+      .getMany();
+
+    return res.json(subs);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
 const router = Router();
 
 router.post('/', user, auth, createSub);
 router.get('/:name', user, getSub);
+router.get('/search/:name', user, searchSubs);
 router.post(
   '/:name/image',
   user,
