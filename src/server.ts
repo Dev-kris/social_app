e831871783d01cd01ab9aa046f2d53357c5993fb
Kris Long <corpkris@gmail.com>
2021-06-15 10:23:20 +0200

    added user activity route

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import authRoutes from './routes/auth';
 import postRoutes from './routes/posts';
 import subRoutes from './routes/subs';
 import miscRoutes from './routes/misc';
+import userRoutes from './routes/users';
 
 import trim from './middleware/trim';
 
@@ -37,6 +38,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/subs', subRoutes);
 app.use('/api/misc', miscRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, async () => {
   console.log('server running at localhost: ' + PORT);
