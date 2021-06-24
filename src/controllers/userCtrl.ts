0211ba5b5de696669f7e0260baca80696391ab3a
Kris Long <corpkris@gmail.com>
2021-06-24 15:22:38 +0200

    refactored users route, added controller

---
+++ src/controllers/userCtrl.ts
@@ -0,0 +1,45 @@
+import { Request, Response, Router } from 'express';
+import Comment from '../entities/Comment';
+import Post from '../entities/Posts';
+import User from '../entities/User';
+
+module.exports.getUserSubmissions = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOneOrFail({
+      where: { username: req.params.username },
+      select: ['username', 'createdAt'],
+    });
+
+    const posts = await Post.find({
+      where: { user },
+      relations: ['comments', 'votes', 'sub'],
+    });
+
+    const comments = await Comment.find({
+      where: { user },
+      relations: ['post'],
+    });
+
+    if (res.locals.user) {
+      posts.forEach((p) => p.setUserVote(res.locals.user));
+      comments.forEach((c) => c.setUserVote(res.locals.user));
+    }
+
+    let submissions: any[] = [];
+    posts.forEach((p) => submissions.push({ type: 'Post', ...p.toJSON() }));
+    comments.forEach((c) =>
+      submissions.push({ type: 'Comment', ...c.toJSON() })
+    );
+
+    submissions.sort((a, b) => {
+      if (b.createdAt > a.createdAt) return 1;
+      if (b.createdAt < a.createdAt) return -1;
+      return 0;
+    });
+
+    return res.json({ user, submissions });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+};
