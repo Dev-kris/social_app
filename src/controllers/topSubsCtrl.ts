33ff957ac54a3a45518b67af3a8f5efc15aace78
Kris Long <corpkris@gmail.com>
2021-06-24 15:59:36 +0200

    refactored misc route into topSubsCtrl controller

---
+++ src/controllers/topSubsCtrl.ts
@@ -0,0 +1,31 @@
+import { Request, Response, Router } from 'express';
+import { getConnection } from 'typeorm';
+import Post from '../entities/Posts';
+import Sub from '../entities/Sub';
+
+import auth from '../middleware/auth'; //make sure to correctly import auth
+import user from '../middleware/user';
+
+const voteController = require('../controllers/voteCtrl');
+
+exports.topSubs = async (_: Request, res: Response) => {
+  try {
+    //TO DO: Test further and add ENV
+    const imageUrlExp = `COALESCE( CONCAT('${process.env.APP_URL}/images/',s.imageUrn) , 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=robohash&f=y')`;
+    const subs = await getConnection()
+      .createQueryBuilder()
+      .select(
+        `s.title, s.name, ${imageUrlExp} as imageUrl, count(p.id) as postCount`
+      )
+      .from(Sub, 's')
+      .leftJoin(Post, 'p', `s.name = p.subName`)
+      .groupBy('s.title, s.name, imageUrl')
+      .orderBy(`postCount`, 'DESC')
+      .limit(5)
+      .execute();
+
+    return res.json(subs);
+  } catch (err) {
+    return res.status(500).json({ error: 'Something went wrong.' });
+  }
+};
--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -7,31 +7,10 @@ import auth from '../middleware/auth'; //make sure to correctly import auth
 import user from '../middleware/user';
 
 const voteController = require('../controllers/voteCtrl');
-
-const topSubs = async (_: Request, res: Response) => {
-  try {
-    //TO DO: Test further and add ENV
-    const imageUrlExp = `COALESCE( CONCAT('${process.env.APP_URL}/images/',s.imageUrn) , 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=robohash&f=y')`;
-    const subs = await getConnection()
-      .createQueryBuilder()
-      .select(
-        `s.title, s.name, ${imageUrlExp} as imageUrl, count(p.id) as postCount`
-      )
-      .from(Sub, 's')
-      .leftJoin(Post, 'p', `s.name = p.subName`)
-      .groupBy('s.title, s.name, imageUrl')
-      .orderBy(`postCount`, 'DESC')
-      .limit(5)
-      .execute();
-
-    return res.json(subs);
-  } catch (err) {
-    return res.status(500).json({ error: 'Something went wrong.' });
-  }
-};
+const topSubsController = require('../controllers/topSubsCtrl');
 
 const router = Router();
 router.post('/vote', user, auth, voteController.vote);
-router.get('/top-subs', topSubs);
+router.get('/top-subs', topSubsController.topSubs);
 
 export default router;
